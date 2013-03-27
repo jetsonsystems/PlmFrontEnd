f2b7293c65f75132e3c1a10b4bc92b2df6b94177
Marek Ryniejski <marekjulian@gmail.com>
2013-03-26 19:05:36 -0700

    initial revisions for incremental all-import

--- Middleman/source/js/app/photo-manager/views/home/library/all-photos.js
+++ Middleman/source/js/app/photo-manager/views/home/library/all-photos.js
@@ -31,19 +31,70 @@ define(
       id: 'photo-manager/home/library/all-photos',
 
       //
+      // status:
       //  STATUS_UNRENDERED: view has not be rendered.
+      //  STATUS_RENDERING: the view is being rendered.
       //  STATUS_RENDERED: the view has been rendered.
-      //  STATUS_RENDERING_IMPORTS: going thru all imports and rendering each one.
-      //  STATUS_INCREMENTALLY_RENDERING_IMPORT: an import within the view, could 
-      //    be the latest or not is being rendered.
       //
       STATUS_UNRENDERED: 0,
-      STATUS_RENDERED: 1,
-      STATUS_RENDERING_IMPORTS: 2,
-      STATUS_INCREMENTALLY_RENDERING_IMPORT: 3,
+      STATUS_RENDERING: 1,
+      STATUS_RENDERED: 2,
+
       status: undefined,
 
+      //
+      // rendering:
+      //  What form of rendering is going on, if any:
+      //
+      //    full: rendering everything!
+      //    incremental: do an incremental render
+      //    dirty: rendering a dirty import
+      //
+      rendering: {
+        full: false,
+        incremental: false,
+        dirty: false
+      },
+
+      //
+      // Set to true, when the view becomes 'dirty'. That is when a message is received
+      // concerning a document change where we cannot immediate update the view.
+      //
       dirty: false,
+      //
+      // Thesee are the importers which will need to be updated in the view at the appropriate
+      // time. This is a mapping from:
+      //
+      //  <importer ID, including the leading '$'> -> {
+      //                                                id: <importer ID>,
+      //                                                status: <importer status>,
+      //                                                created_at: <created at> }
+      //
+      //    <importer status> ::= CREATED | UPDATED | DELETED
+      //
+      IMPORTER_STATUS_CREATED: 0,
+      IMPORTER_STATUS_UPDATED: 1,
+      IMPORTER_STATUS_DELETED: 2,
+
+      dirtyImporters: {},
+
+      _updateDirtyImporters: function(id, status, created_at) {
+        if (_.has(this.dirtyImporters, id)) {
+          if (status) {
+            this.dirtyImporters[id].status = status;
+          }
+          if (created_at) {
+            this.dirtyImporters[id].created_at = created_at;
+          }
+        }
+        else {
+          this.dirtyImporters[id] = {
+            id: id,
+            status: status,
+            created_at: created_at
+          };
+        }
+      },
 
       //
       // The collection of importers representing this view.
@@ -89,7 +140,6 @@ define(
             },
             done: function() {
               console.log(that.id + '.AllPhotosView.render: rendered all importers...');
-              that.status = that.STATUS_RENDERED;
               that.trigger(that.id + ":rendered");              
             }});
         };
@@ -139,7 +189,8 @@ define(
         }
         else {
           console.log(this.id + '._renderImports: Rendering ' + this.importers.length + ' imports...');
-          this.status = this.STATUS_RENDERING_IMPORTS;
+          this.status = this.STATUS_RENDERING;
+          this.rendering.full = true;
           var toRender = this.importers.toArray();
 
           function renderImport(importer) {
@@ -156,8 +207,10 @@ define(
               if (toRender.length > 0) {
                 renderImport(toRender.shift());
               }
-              else if (options && options.done) {
-                options.done();
+              else {
+                that.rendering.full = false;
+                that.status = that.STATUS_RENDERED;
+                !options || !options.done || options.done();
               }
             };
             var onError = function() {
@@ -167,6 +220,10 @@ define(
               }
               if (toRender.length > 0) {
                 renderImport(toRender.shift());
+              }
+              else {
+                that.rendering.full = false;
+                that.status = that.STATUS_RENDERED;
               }
             };
             importerImages.fetch({success: onSuccess,
@@ -181,10 +238,13 @@ define(
       //
       _startIncrementallyRenderingImport: function(importer) {
         var that = this;
+        //
+        // We only allow starting an incremental rendering when nothing else is going on.
+        //
         if (that.status === that.STATUS_RENDERED) {
           console.log(that.id + '._startIncrementallyRenderingImport: Starting to incrementally render import, importer - ' + JSON.stringify(importer));
 
-          that.status = that.STATUS_INCREMENTALLY_RENDERING_IMPORT;
+          that.status = that.STATUS_RENDERING;
 
           that.importRenderingInc = new ImporterModel(importer);
           that.importRenderingIncImages = new ImportersImagesCollection(undefined, {importerId: importer.id});
@@ -213,14 +273,14 @@ define(
             that.$importRenderingInc = $('#' + importElId);
             console.log(that.id + '._startIncrementallyRenderingImport: compiled initial template for import, element ID - ' + importElId + ', element found - ' + that.$importRenderingInc.length);
           }
-          that.status = that.STATUS_INCREMENTALLY_RENDERING_IMPORT;
+          that.rendering.incremental = true;
         }
         return that;
       },
 
       _addToIncrementalImportRender: function(image) {
         var that = this;
-        if (that.status === that.STATUS_INCREMENTALLY_RENDERING_IMPORT) {
+        if (that.rendering.incremental) {
           console.log(that.id + '._addToIncrementalImportRender: Adding image to import w/ id - ' + image.id + ', to view.');
           //
           // Add to the collection.
@@ -240,13 +300,122 @@ define(
         return that;
       },
 
-      _finishImportImportRender: function(importer) {
-        if (this.status === this.STATUS_INCREMENTALLY_RENDERING_IMPORT) {
+      _finishIncrementalImportRender: function(importer) {
+        if (this.rendering.incremental) {
           this.$importRenderingInc.find(".imported-timestamp").text(" Imported: " + importer.completed_at);
           this.importRenderingInc = undefined;
           this.importRenderingIncImages = undefined;
           this.$importRenderingInc = undefined;
-          this.status = this.STATUS_RENDERED;
+          this.rendering.incremental = false;
+          if (!this.rendering.full && !this.rendering.dirty) {
+            this.status = this.STATUS_RENDERED;
+          }
+          else {
+            this.status = this.STATUS_RENDERING;
+          }
+        }
+      },
+
+      //
+      // _renderDirtyImport: Render a single dirty import.
+      //  The process involves:
+      //    1. Removing it from the set of dirtyImporters.
+      //    2. Fetching the import
+      //      2.a. onSuccess: 
+      //        - Finding the element to insert it after.
+      //        - insert it.
+      //        - invoke an onSuccess callback.
+      //      3.b. onError: 
+      //        - Re-insert it into the set of dirty importers.
+      //        - invoke an onError callback.
+      //
+      _renderDirtyImport: function(importerId, options) {
+        var that = this;
+
+        //
+        // Don't allow us to do if a full render is inprogess:
+        //
+        if (that.rendering.full) {
+          !options || !options.error || options.error('Full render in progress');
+          return;
+        }
+        if (_.has(that.dirtyImporters, importerId)) {
+          var importerToDo = that.dirtyImporters[importerId];
+          delete that.dirtyImporters[importerToDo.id];
+
+          that.status = this.STATUS_RENDERING;
+          that.rendering.dirty = true;
+
+          var importerImages = new ImportersImagesCollection(undefined, {importerId: importerId});
+
+          var onSuccess = function(collection, response) {
+            !Plm.debug || console.log(debugPrefix + '._renderDirtyImport.onSuccess: Have importer w/ id - ' + importerImages.importerId + ', response - ' + JSON.stringify(response));
+            var iAttr = _.clone(response.importer);
+            delete iAttr.images;
+            !Plm.debug || console.log(debugPrefix + '._renderDirtyImport.onSuccess: Create importer model with attr - ' + iAttr);
+            var importer = new ImporterModel(iAttr);
+            var compiledTemplate = _.template(importTemplate, { importer: importer,
+                                                                importImages: importerImages,
+                                                                imageTemplate: importImageTemplate,
+                                                                _: _ });
+            //
+            // Find the element to insert after, or if it exists, do a replace.
+            //
+            that.$el.find('.imports').append(compiledTemplate);
+            that.rendering.dirty = false;
+            if (!that.rendering.full && !that.rendering.incremental) {
+              that.status = that.STATUS_RENDERED;
+            }
+            !options || !options.success || options.success();
+          };
+          var onError = function() {
+            Plm.showFlash('There was an unexpected error retrieving images for one of your imports!');
+            that.rendering.dirty = false;
+            if (!that.rendering.full && !that.rendering.incremental) {
+              that.status = that.STATUS_RENDERED;
+            }
+            !options || !options.error || options.error('There was an unexpected retrieving images for an import!');
+          };
+
+          importerImages.fetch({success: onSuccess,
+                                error: onError});
+        }
+        else {
+          if (_.has(options, 'error')) {
+            !options || !options.error || options.error('Importer with id ' + importerId + ' not found!');
+          }
+        }
+      },
+
+      _renderDirtyImports: function() {
+        var that = this;
+        var doOne = function(onSuccess, onError) {
+          var toDo = _.sortBy(that.dirtyImporters,
+                              function(v, k) { return v.created_at; });
+          if (_.size(toDo) > 0) {
+            var importerToDo = _.first(toDo);
+            that._renderDirtyImport(importerToDo.id,
+                                    {success: onSuccess,
+                                     error: onError});
+          }
+          else {
+            onError('No importers');
+          }
+        };
+        var onSuccess = function(id) {
+          if (_.size(that.dirtyImporters) > 0) {
+            doOne(onSuccess, onError);
+          }
+        };
+        var onError = function(err) {
+          if (_.size(that.dirtyImporters) > 0) {
+            window.setTimeout(function() {
+              doOne(onSuccess, onError);
+            }, 1000);
+          }
+        };
+        if (_.size(this.dirtyImporters) > 0) {
+          doOne(onSuccess, onError);
         }
       },
 
@@ -255,9 +424,13 @@ define(
         MsgBus.subscribe('_notif-api:' + '/importers',
                          'import.started',
                          function(msg) {
-                           !Plm.debug || console.log(that.id + '._respondToEvents: import started, msg - ' + JSON.stringify(msg));
-                           if (that.status === that.STATUS_INCREMENTALLY_RENDERING_IMPORT) {
+                           !Plm.debug || console.log(that.id + '._respondToEvents: import started, rendering incremental - ' + that.rendering.incremental);
+                           !Plm.debug || !Plm.verbose || console.log(that.id + '._respondToEvents: import started, msg - ' + JSON.stringify(msg));
+                           if (that.rendering.incremental) {
                              that.dirty = true;
+                             that._updateDirtyImporters(msg.data.id,
+                                                        that.IMPORTER_STATUS_CREATED,
+                                                        msg.data.created_at);
                            }
                            else {
                              that._startIncrementallyRenderingImport(msg.data);
@@ -267,26 +440,29 @@ define(
                          'import.image.saved',
                          function(msg) {
                            !Plm.debug || console.log(that.id + '._respondToEvents: import image saved, msg - ' + JSON.stringify(msg));
-                           if ((that.status === that.STATUS_INCREMENTALLY_RENDERING_IMPORT) && (that.importRenderingInc.id === msg.data.id)) {
+                           if (that.rendering.incremental && (that.importRenderingInc.id === msg.data.id)) {
                              that._addToIncrementalImportRender(msg.data.doc);
                            }
                            else {
                              that.dirty = true;
+                             that._updateDirtyImporters(msg.data.id,
+                                                        that.IMPORTER_STATUS_UPDATED);
                            }
                          });
         MsgBus.subscribe('_notif-api:' + '/importers',
                          'import.completed',
                          function(msg) {
-                           !Plm.debug || console.log(that.id + '._respondToEvents: import completed, msg - ' + JSON.stringify(msg));
-                           if ((that.status === that.STATUS_INCREMENTALLY_RENDERING_IMPORT) && (that.importRenderingInc.id === msg.data.id)) {
-                             that._finishImportImportRender(msg.data);
-                             !that.dirty || that._reRender();
-                           }
-                           else if (that.status === that.STATUS_INCREMENTALLY_RENDERING_IMPORT) {
-                             that.dirty = true;
+                           !Plm.debug || console.log(debugPrefix + '._respondToEvents: import completed, rendering incremental - ' + that.rendering.incremental);
+                           !Plm.debug || !Plm.verbose || console.log(debugPrefix + '._respondToEvents: import completed, msg - ' + JSON.stringify(msg));
+                           if (that.rendering.incremental && (that.importRenderingInc.id === msg.data.id)) {
+                             that._finishIncrementalImportRender(msg.data);
                            }
                            else {
-                             that._reRender();
+                             that.dirty = true;
+                             that._updateDirtyImporters(msg.data.id,
+                                                        that.IMPORTER_STATUS_UPDATED,
+                                                        msg.data.created_at);
+                             that._renderDirtyImport(msg.data.id);
                            }
                          });
 
@@ -300,7 +476,32 @@ define(
                          function(msg) {
                            !Plm.debug || console.log(debugPrefix + '._respondToEvents: doc change event, event - ' + msg.event);
                            !Plm.debug || !Plm.verbose || console.log(debugPrefix + '._respondToEvents: msg.data - ' + msg.data);
-                           that.dirty = true;
+                           var parsedEvent = msg.event.split('.');
+                           if (parsedEvent[1] === 'importer') {
+                             that.dirty = true;
+                             if (parsedEvent[2] === 'created') {
+                               that._updateDirtyImporters(msg.data.doc.id,
+                                                          that.IMPORTER_STATUS_CREATED,
+                                                          msg.data.doc.created_at);
+                             }
+                             else if (parsedEvent[2] === 'deleted') {
+                               that._updateDirtyImporters(msg.data.doc.id,
+                                                          that.IMPORTER_STATUS_DELETED);
+                             }
+                             else {
+                               that._updateDirtyImporters(msg.data.doc.id,
+                                                          that.IMPORTER_STATUS_UPDATED,
+                                                          msg.data.doc.created_at);
+                             }
+                           }
+                           else if (parsedEvent[1] === 'image') {
+                             //
+                             // Any change to an image implies the corresponding importer has changed.
+                             //
+                             that.dirty = true;
+                             that._updateDirtyImporters(msg.data.doc.importer_id,
+                                                        that.IMPORTER_STATUS_UPDATED);
+                           }
                          });
 
         MsgBus.subscribe('_notif-api:' + '/storage/synchronizers',
@@ -309,9 +510,7 @@ define(
                            !Plm.debug || console.log(debugPrefix + '._respondToEvents: sync.completed event...');
                            if (that.dirty) {
                              !Plm.debug || console.log(debugPrefix + '._respondToEvents: sync.completed, view is dirty...');
-                             if (that.status !== that.STATUS_INCREMENTALLY_RENDERING_IMPORT) {
-                               that._reRender();
-                             }
+                             that._renderDirtyImports();
                            }
                          });
 
