55077cf8311738615adfa0acdc6f65e1f779be31
Marek J. Ryniejski <marekjulian@gmail.com>
2013-08-24 13:47:55 -0700

    Fix for issue 8002

--- Middleman/source/js/app/photo-manager/collections/importers.js
+++ Middleman/source/js/app/photo-manager/collections/importers.js
@@ -15,15 +15,11 @@ define(
     //    models: stardard.
     //    options:
     //      numToFetch: number to return.
-    //      filterWithoutStartedAt: don't return any which do NOT have a started_at attributed.
-    //        These are imports which failed to trigger for some reason.
     //
     var ImportersCollection = Backbone.Collection.extend({
       model: ImporterModel,
 
       numToFetch: undefined,
-
-      filterWithoutStartedAt: false,
 
       //
       // initialize:
@@ -37,18 +33,13 @@ define(
           if (_.has(options, 'numToFetch') && options.numToFetch) {
             this.numToFetch = options.numToFetch;
           }
-          if (_.has(options, 'filterWithoutStartedAt')) {
-            this.filterWithoutStartedAt = options.filterWithoutStartedAt;
-          }
         }
       },
 
       url: function() {
         var url = '/api/media-manager/v0/importers';
-        if (!this.filterWithoutStartedAt) {
-          if (this.numToFetch) {
-            url = url + '?n=' + this.numToFetch;
-          }
+        if (this.numToFetch) {
+          url = url + '?n=' + this.numToFetch;
         }
         return url;
       },
@@ -59,19 +50,7 @@ define(
       //  proper number.
       //
       parse: function(response) {
-        if (this.filterWithoutStartedAt) {
-          var filtered = _.filter(response.importers,
-                                  function(importer) { return _.has(importer, 'started_at'); });
-          if (this.numToFetch) {
-            return _.first(filtered, this.numToFetch);
-          }
-          else {
-            return filtered;
-          }
-        }
-        else {
-          return response.importers;
-        }
+        return response.importers;
       }
 
     });
